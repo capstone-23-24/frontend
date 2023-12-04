427fa3eee5bcbeea14402dd70577ba04018ee15f
Aadhieaswar <asenthi3@uwo.ca>
2023-12-04 12:02:29 -0500

    Added CSS for better UI

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,8 @@
+import './Post.css';
+
 const Post = ({ post }) => {
     return (
-        <article>
+        <article class='post-container'>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
             <p>Post ID: {post.id}</p>
--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import './LandingPage.css';
 import { Upload, Button, message, List} from 'antd';
 import { getPosts } from '../../api/axios'
-
-import { UploadOutlined } from '@ant-design/icons';
+import { InboxOutlined } from '@ant-design/icons';
 // import SearchBar from '../SearchBar'
 import SearchList from '../SearchList'
 import Results from '../Results/Results'
+
+const { Dragger } = Upload;
 
 const LandingPage = () => {
   const [posts, setPosts] = useState([])
@@ -44,6 +46,7 @@ const LandingPage = () => {
     }));
 
     setResults(processedResults);
+    console.log(posts);
   };
 
   return (
@@ -51,30 +54,43 @@ const LandingPage = () => {
       {/* <SearchBar/> */}
       {/* <h1>G23 Capstone RI</h1> */}
       
-      <Upload {...props} fileList={fileList}>
-        <Button icon={<UploadOutlined />}>Select PDFs to Upload</Button>
-      </Upload>
-      
-      <Button type="primary" onClick={handleUpload} style={{ marginTop: '16px' }}>
-        Start Processing
-      </Button>
-
-      {results.length > 0 && (
-        <div style={{ marginTop: '20px' }}>
-          <h2>Processing Results</h2>
-          <List
-            dataSource={results}
-            renderItem={item => (
-              <List.Item>
-                <List.Item.Meta
-                  title={item.name}
-                  description={item.status}
-                />
-              </List.Item>
-            )}
-          />
+      <div class="form-container">
+        <div class="upload-form">
+          <h3>Upload Your Document Here</h3>
+          {/* <Upload {...props} fileList={fileList}>
+            <Button icon={<UploadOutlined />}>Select PDFs to Upload</Button>
+          </Upload> */}
+          <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">Click or drag file to this area to upload</p>
+            <p className="ant-upload-hint">
+              Support for a single or bulk upload.
+            </p>
+          </Dragger>
+          
+          <Button type="primary" onClick={handleUpload} style={{ marginTop: '16px' }}>
+            Start Processing
+          </Button>
+          {results.length > 0 && (
+            <div style={{ marginTop: '20px' }}>
+              <h2>Processing Results</h2>
+              <List
+                dataSource={results}
+                renderItem={item => (
+                  <List.Item>
+                    <List.Item.Meta
+                      title={item.name}
+                      description={item.status}
+                    />
+                  </List.Item>
+                )}
+              />
+            </div>
+          )}
         </div>
-      )}
+      </div>
 
       <SearchList searchResults={searchResults} />
       <Results caseTitle={"Sample Case"} predictedTags={["tag1", "tag2", "tag3"]} caseInformation={"Sample Case Information"}/>
