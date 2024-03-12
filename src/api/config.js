fc476c452663e63c593c9cd828524b2c92ff646a
Anuj Kulkarni <anujkulkarni@Anujs-MacBook-Pro.local>
2024-03-12 19:47:38 -0400

    conflict removal

--- src/api/config.js
+++
@@ -1,9 +0,0 @@
-const configFile = {
-    // will have to convert the keys to process.env
-    region: JSON.stringify("us-east-1"),
-    accessKeyId: JSON.stringify('AKIAVRUVQ7IMGFJFDSHG'),
-    secretAccessKey: JSON.stringify("CQtV+a6UNWzEtQnN4U6nseOLqCELSS2Bw3hQL1bN3sxCM" ),
-
-}
-
-export default configFile;
